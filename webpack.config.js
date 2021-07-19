10d5f6a72fc3f46ef441f970dd5226b94407883c
gmzadev <gmzadev@gmail.com>
2021-07-19 03:45:37 -0400

    cambios de 3:45 am
    conecion con firebase, funciona el html

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');const basePath = __dirname;
+const distPath = 'dist';
+ 
+const indextInput = './public/index.html';
+const indexOutput = 'index.html';const webpackInitConfig = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js']
+    },
+    entry: {
+        app: ['@babel/polyfill', './src/index.js'],
+    },
+    output: {
+        path: path.join(basePath, distPath),
+        filename: '[chunkhash][name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: indexOutput, 
+            template: indextInput,
+        })
+    ]
+};module.exports = webpackInitConfig;
